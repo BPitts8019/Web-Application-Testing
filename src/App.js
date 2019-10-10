9c6de08fdfcc19393d0eb143efc92ff4b896b932
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-10 16:54:33 -0700

    Added react testing library and extended jest

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the app!</h1>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <header className="App-header">
+            <h1>Welcome to the app!</h1>
+         </header>
+      </div>
+   );
 }
 
 export default App;
